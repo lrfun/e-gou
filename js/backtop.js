2546e0b3f149311236edf249c53482bca13b37f3
lrfun <luorongfang126@126.com>
2015-04-16 11:38:39 +0800

    backtop js

--- js/backtop.js
+++ js/backtop.js
@@ -1,6 +1,39 @@
 // JavaScript Document
 
-$("#top").click(function(){
-	var height= document.body.scrollTop||document.documentElement.scrollTop;
-	document.body.scrollTop=document.documentElement.scrollTop=0;
+window.onload=function(){
+	//获取页面可视区高度
+	var clientHeight=document.documentElement.clientHeight||document.body.clientHeight;
+	
+	$("#top").hide();
+	//滚动条滚动时触发
+	window.onscroll=function(){
+		var height= document.body.scrollTop||document.documentElement.scrollTop;
+
+		if (height>=clientHeight){
+			$("#top").show();
+
+		}
+		else{
+			$("#top").hide();
+		}
+
+	}
+
+	$("#top").click(function(){
+
+		timer=setInterval(function(){
+
+			//获取滚动条距顶部距离
+			var height= document.body.scrollTop||document.documentElement.scrollTop;
+			var ispeed=height/4
+			document.body.scrollTop=document.documentElement.scrollTop=height-ispeed;
+
+			//判断距顶部距离为0时清除函数
+			if(height==0){
+				clearInterval(timer);
+			}
+		},30)
+		
 });
+}
+
